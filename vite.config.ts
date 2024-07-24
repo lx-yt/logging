3500ee66d139b36b5e2f6f0c070fa490c9c4e058
Lexyth <marfy@web.de>
2024-07-24 06:09:59 +0200

    build: Remove build.lib.name
    The formats don't include 'umd' or 'iife' so the name won't be used.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
-import { defineConfig, mergeConfig } from "vite";
+import { defineConfig } from "vite";
 
 // using relative path to avoid error: TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts"
 import baseConfig from "./node_modules/@lx-yt/vite-config-default/vite.config";
 
-export default defineConfig(
-  mergeConfig(baseConfig, { build: { lib: { name: "Logging" } } })
-);
+export default defineConfig(baseConfig);
